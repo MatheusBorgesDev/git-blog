1d497db9d0c928dd93f2fa54059664dc39440f43
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-08-26 23:05:29 -0300

    chore: :lipstick: adjust header style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import Logo from "../assets/logo.svg";
 
 export function Header() {
   return (
-    <header className="bg-cover-image flex h-80 w-full justify-center bg-cover bg-center">
-      <img src={Logo} className="-mt-12 w-40" alt="" />
+    <header className="flex h-80 w-full justify-center bg-cover-image bg-cover bg-center">
+      <img src={Logo} className="-mt-16 w-40" alt="" />
     </header>
   );
 }
