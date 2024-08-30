c699c6c19f9ac3eb2637cf24754e31d62c0f7a0c
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-08-30 16:11:17 -0300

    feat: :sparkles: ensure that the issues' content is displayed in markdown format

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,3 +1,5 @@
+import ReactMarkDown from "react-markdown";
+
 interface PostCardProps {
   title: string;
   body: string;
@@ -6,13 +8,15 @@ interface PostCardProps {
 
 export function PostCard({ title, body, createdAt }: PostCardProps) {
   return (
-    <div className="flex h-64 w-full flex-col gap-5 rounded-lg bg-basePost p-8">
+    <div className="flex h-[260px] w-full flex-col gap-5 rounded-lg bg-basePost p-8">
       <div className="flex gap-4">
         <h3 className="text-xl font-semibold text-baseTitle">{title}</h3>
         <span className="text-nowrap text-baseSpan">{createdAt}</span>
       </div>
 
-      <p className="overflow-hidden text-ellipsis text-baseText">{body}</p>
+      <div className="overflow-hidden text-ellipsis text-baseText">
+        <ReactMarkDown>{body}</ReactMarkDown>
+      </div>
     </div>
   );
 }
