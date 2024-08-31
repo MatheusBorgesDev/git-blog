0fe2c7d4c34b4ff07bb5a27e3939b5d27481037e
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-08-30 23:53:55 -0300

    chore: :recycle: turn post profile dynamic

--- src/pages/post/components/post-profile.tsx
+++ src/pages/post/components/post-profile.tsx
@@ -8,37 +8,48 @@ import {
 
 import { ProfileInfo } from "../../../components/profile-info";
 import { TextLink } from "../../../components/text-link";
+import { gitHubUser } from "../../../contexts/github-data";
 
-export function PostProfile() {
+interface PostProfileProps {
+  postTitle: string;
+  postLink: string;
+  postCommentsAmount: number;
+  createdAt: string;
+}
+
+export function PostProfile({
+  postTitle,
+  postLink,
+  postCommentsAmount,
+  createdAt,
+}: PostProfileProps) {
   return (
     <div className="-mt-24 flex w-full flex-col gap-5 rounded-xl bg-baseProfile px-10 py-8 text-blue shadow-lg">
       <div className="flex w-full items-center justify-between">
-        <TextLink href="/">
+        <TextLink navigateTo="/">
           <FaChevronLeft size={12} /> Voltar
         </TextLink>
 
-        <TextLink href="https://github.com/MatheusBorgesDev" target="_blank">
+        <TextLink navigateTo={postLink} target="_blank">
           Ver no GitHub <FaExternalLinkAlt size={12} />
         </TextLink>
       </div>
 
       <div>
         <div className="space-y-3">
-          <h1 className="text-2xl font-bold text-baseTitle">
-            JavaScript data types and data structures
-          </h1>
+          <h1 className="text-2xl font-bold text-baseTitle">{postTitle}</h1>
 
           <div className="flex gap-6">
             <ProfileInfo icon={FaGithub}>
-              <span>MatheusBorgesDec</span>
+              <span>{gitHubUser}</span>
             </ProfileInfo>
 
             <ProfileInfo icon={FaCalendarDay}>
-              <span>Há 1 dia</span>
+              <span>{createdAt}</span>
             </ProfileInfo>
 
             <ProfileInfo icon={FaComment}>
-              <span>5 Comentários</span>
+              <span>{postCommentsAmount} comentário(s)</span>
             </ProfileInfo>
           </div>
         </div>
