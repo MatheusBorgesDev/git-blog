22f62bb0a533922660d30e936eae367c8359fdce
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-08-30 23:46:57 -0300

    feat: :sparkles: turn the post card component into a link

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,14 +1,24 @@
 import ReactMarkDown from "react-markdown";
+import { Link } from "react-router-dom";
 
 interface PostCardProps {
   title: string;
   body: string;
   createdAt: string;
+  issueNumber: number;
 }
 
-export function PostCard({ title, body, createdAt }: PostCardProps) {
+export function PostCard({
+  title,
+  body,
+  createdAt,
+  issueNumber,
+}: PostCardProps) {
   return (
-    <div className="flex h-[260px] w-full flex-col gap-5 rounded-lg bg-basePost p-8">
+    <Link
+      to={`/post/${issueNumber}`}
+      className="flex h-[260px] w-full cursor-pointer flex-col gap-5 rounded-lg bg-basePost p-8 transition hover:scale-[1.05] hover:bg-basePost/80"
+    >
       <div className="flex gap-4">
         <h3 className="text-xl font-semibold text-baseTitle">{title}</h3>
         <span className="text-nowrap text-baseSpan">{createdAt}</span>
@@ -17,6 +27,6 @@ export function PostCard({ title, body, createdAt }: PostCardProps) {
       <div className="overflow-hidden text-ellipsis text-baseText">
         <ReactMarkDown>{body}</ReactMarkDown>
       </div>
-    </div>
+    </Link>
   );
 }
